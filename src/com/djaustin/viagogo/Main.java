15432ce09e1f6a41d23700597de34ad33a0e2b1e
Dan Austin <dja470@bham.ac.uk>
2017-10-06 12:19:24 +0100

    Add comment to Main class

--- src/com/djaustin/viagogo/Main.java
+++ src/com/djaustin/viagogo/Main.java
@@ -6,6 +6,10 @@ import com.djaustin.viagogo.seed.Generator;
 import java.util.Optional;
 
 public class Main {
+    /**
+     * Generates a random 20x20 grid of events and tickets then finds the 5 closest events to a point provided as a command line argument
+     * @param args The command line arguments used to run the program. The user coordinate should be the first argument in the form x,y
+     */
     public static void main(String[] args) {
         // Generate seed data
         Generator generator = new Generator();
@@ -49,6 +53,11 @@ public class Main {
         }
     }
 
+    /**
+     * Validates the command line arguments to ensure that the first argument is a coordinate in the correct format.
+     * @param args The command line arguments used to run the program.
+     * @return Optional Coordinate if it was possible to parse the user input. Returns empty Optional if parsing was unsuccessful.
+     */
     public static Optional<Coordinate> validateAndParseUserInput(String[] args){
         if (args.length < 1){
             return Optional.empty();
